78b00d42df54d00955cdb7c9b1c5b7563eb5ee15
Eugene Teoh <weichien.teoh@dyson.com>
2023-02-11 12:58:23 +0000

    Add nvim config

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,70 @@
+local function ensure_packer()
+  local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+
+  if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+    print('Installing packer...')
+    local packer_url = 'https://github.com/wbthomason/packer.nvim'
+    vim.fn.system({'git', 'clone', '--depth', '1', packer_url, install_path})
+    print('Done.')
+
+    vim.cmd('packadd packer.nvim')
+    return true
+  end
+
+  return false
+end
+
+-- You can "comment out" the line below after packer is installed
+local install_plugins = ensure_packer()
+-- local install_plugins = true
+
+require('packer').startup(function(use)
+  use {'wbthomason/packer.nvim'}
+  use {'linty-org/readline.nvim'}
+  use({
+    "gbprod/cutlass.nvim",
+    config = function()
+      require("cutlass").setup({
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      })
+    end
+  })
+
+  if install_plugins then
+    require('packer').sync()
+  end
+end)
+
+if install_plugins then
+  print '=================================='
+  print '    Plugins will be installed.'
+  print '      After you press Enter'
+  print '    Wait until Packer completes,'
+  print '       then restart nvim'
+  print '=================================='
+  return
+end
+
+-- Basic clipboard interaction
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+vim.keymap.set({'n', 'x'}, '<C-c>', '"+y', {noremap=true, silent=true})
+vim.keymap.set({'n', 'x'}, '<C-x>', '+c', {noremap=true, silent=true})
+vim.keymap.set({'n', 'x'}, '<C-v>', '"+p', {noremap=true, silent=true})
+vim.keymap.set('i', '<C-v>', '<C-r><C-o>+')
+
+-- readline.nvim
+local readline = require 'readline'
+vim.keymap.set('!', '<M-f>', readline.forward_word)
+vim.keymap.set('!', '<M-b>', readline.backward_word)
+vim.keymap.set('!', '<C-a>', readline.beginning_of_line)
+vim.keymap.set('!', '<C-e>', readline.end_of_line)
+vim.keymap.set('!', '<M-d>', readline.kill_word)
+vim.keymap.set('!', '<M-BS>', readline.backward_kill_word)
+vim.keymap.set('!', '<C-w>', readline.unix_word_rubout)
+vim.keymap.set('!', '<C-k>', readline.kill_line)
+vim.keymap.set('!', '<C-u>', readline.backward_kill_line)
